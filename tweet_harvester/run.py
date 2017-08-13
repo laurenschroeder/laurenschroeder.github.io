890aaaa9da27f6906a39b67d4c5e5e02cfb92a1e
laurenschroeder <schroe35@gmail.com>
2017-08-13 18:33:16 -0500

    add tweet harvester

---
+++ tweet_harvester/run.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 10 09:31:09 2017
+
+@author: schro
+"""
+
+from tweet_harvester import app
+# 'app' originates from the line 'app = Flask(__name__)'
+app.run(port=8080)
